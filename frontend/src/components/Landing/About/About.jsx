07985c89227f956eb8c3a1028027658a20aecd73
Soumyajit Das <soumyajitdhoni@gmail.com>
2024-06-15 02:33:17 +0530

    access-page-updates

---
+++ frontend/src/components/Landing/About/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './About.css'
+const About = () => {
+  return (
+    <div className='about'>
+
+  
+<div className='about_head1'>
+Welcome to our Item Exchange Hub
+</div>
+<div className='about_head2'> A dedicated marketplace for our institution, connecting students to buy and sell.</div>
+<div className='about_head1'>
+Second hand items are now gonna find new homes directly!
+</div>
+<div className='about_content'>
+<div> It’s a platform for a wide variety of items - from electronic items to engineering instruments. We believe in extending the lifecycle of products, promoting a culture of reusing. Sellers can list their items with a few simple clicks, and buyers can browse through the products easily. We prioritize safety and trust, ensuring a secure environment for our users. 
+We don’t handle payments directly yet. For the time being, parties can arrange payment and delivery outside the platform after negotiation.
+Join us in this journey of sustainability and smart consumption. 
+</div>
+</div>
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/components/LoginSignup/Getstarted/Getstarted.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import './Getstarted.css'
+import { FaRecycle ,FaShopify } from "react-icons/fa6";
+function Getstarted() {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  return (
+    <div className="getstarted-page">
+        
+      <div className="sidebar">
+      <div className="landing-navbar-left-logo">
+                Campus Recycle
+            </div>
+        <div className='getstarted-page-headtext'>How do you want to use Campus Recycle?</div>
+        <div className='getstarted-page-subheadtext'>We’ll personalize your setup experience accordingly.</div>
+       < div className="getstarted-button">
+        <button
+          className={`sidebar-button ${activeButton === 'button1' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('button1')}
+        >
+       
+            <div className='sidebar-button-icon'><FaRecycle /></div>
+            <div className='sidebar-button-text'>
+            <p className='sidebar-button-subtext'> I’m here to hire tech talent</p>
+            <p>  Evaluate tech skills at scale</p>
+            </div>
+        
+        </button>
+        <button
+          className={`sidebar-button ${activeButton === 'button2' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('button2')}
+        >
+             
+            <div className='sidebar-button-icon'><FaShopify  /></div>
+            <div className='sidebar-button-text'>
+            <p className='sidebar-button-subtext'> I’m here to practice and prepare</p>
+            <p> Solve problems and learn new skills</p>
+            </div>
+         
+           
+
+        </button>
+        {activeButton === null && (
+          <div className='create_account_button'>
+             <button className="defbutton">
+             Create account
+      <div className="arrow-wrapper">
+        <div className="arrow"></div>
+      </div>
+    </button>
+          </div>
+        )}
+        {activeButton === 'button1' && (
+          <div className='create_account_button'>
+              <button className="button">
+              Create account
+      <div className="arrow-wrapper">
+        <div className="arrow"></div>
+      </div>
+    </button>
+          </div>
+        )}
+        {activeButton === 'button2' && (
+          <div className='create_account_button'>
+             <button className="button">
+             Create account
+      <div className="arrow-wrapper">
+        <div className="arrow"></div>
+      </div>
+    </button>
+          </div>
+        )}
+        </div>
+      </div>
+      <div className="content1">
+        {activeButton === 'button1' && (
+          <div>
+            Content for Button 1
+          </div>
+        )}
+        {activeButton === 'button2' && (
+          <div>
+            Content for Button 2
+          </div>
+        )}
+      
+      </div>
+    </div>
+  );
+}
+
+export default Getstarted;
